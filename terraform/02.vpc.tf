de184b0730b604862a572583da8f5e9ade44cbb7
hdd732960 <hdd732960@gmail.com>
2025-01-21 13:21:57 +0900

    rm terraform

--- terraform/02.vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "raku_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "raku-vpc"
-  }
-}
--- terraform/04.igw.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "raku_igw" {
-  vpc_id = aws_vpc.raku_vpc.id
-  tags = {
-    Name = "raku-igw"
-  }
-}
